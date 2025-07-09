a3c0901c0bc7e53b6bc5dcb761e920b20efb3f8f
DYSTOpyy <bcd.saltel@gmail.com>
2025-07-09 15:28:01 +0200

    refactor: matrix pub

--- src/consts.rs
+++ src/consts.rs
@@ -1,8 +1,8 @@
 pub const SIZE: usize = 8;
-pub const MAX_DEPTH: usize = 6; // Maximum depth for the AI search algorithms
+pub const MAX_DEPTH: usize = 5; // Maximum depth for the AI search algorithms
 pub const ULTRA_THREADING: bool = false; // Enable or disable ultra threading for AI calculations
 
-pub const MATRIX_A: [[isize; 8]; 8] = [
+pub const MATRIX_A: [[isize; SIZE]; SIZE] = [
     [100, -20, 10, 5, 5, 10, -20, 100],
     [-20, -50, -2, -2, -2, -2, -50, -20],
     [10, -2, -1, -1, -1, -1, -2, 10],
@@ -13,7 +13,7 @@ pub const MATRIX_A: [[isize; 8]; 8] = [
     [100, -20, 10, 5, 5, 10, -20, 100],
 ];
 
-pub const MATRIX_B: [[isize; 8]; 8] = [
+pub const MATRIX_B: [[isize; SIZE]; SIZE] = [
     [500, -150, 30, 10, 10, 30, -150, 500],
     [-150, -250, 0, 0, 0, 0, -250, -150],
     [30, 0, 1, 2, 2, 1, 0, 30],
